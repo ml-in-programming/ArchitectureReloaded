de5725dd08b3597fbe74d045cd46701c5d35e859
ArtyomLobanov <lobanov98@yandex.ru>
2017-07-25 13:50:02 +0300

    Not asymptotic optimizations of HAC

--- src/main/java/org/ml_methods_group/algorithm/HAC.java
+++ src/main/java/org/ml_methods_group/algorithm/HAC.java
@@ -16,7 +16,6 @@
 
 package org.ml_methods_group.algorithm;
 
-import com.sixrr.metrics.MetricCategory;
 import org.jetbrains.annotations.NotNull;
 import org.ml_methods_group.algorithm.entity.Entity;
 import org.ml_methods_group.algorithm.entity.EntitySearchResult;
@@ -43,19 +42,18 @@ public class HAC extends Algorithm {
         newClassCount = 0;
         Stream.of(entities.getClasses(), entities.getMethods(), entities.getFields())
                 .flatMap(List::stream)
-                .map(Community::new)
+                .map(this::singletonCommunity)
                 .forEach(communities::add);
 
         for (Community first : communities) {
-            final Entity representative = getRepresentative(first);
+            final Entity representative = first.entities.get(0);
             for (Community second : communities) {
                 if (first == second) {
                     break;
                 }
-                final double distance = representative.distance(getRepresentative(second));
-                createAndInsertTriple(distance, first, second);
+                final double distance = representative.distance(second.entities.get(0));
+                insertTriple(distance, first, second);
             }
-            System.out.println("Distances calculated for " + first.name);
         }
     }
 
@@ -64,14 +62,10 @@ public class HAC extends Algorithm {
 
         while (!heap.isEmpty()) {
             final Triple minTriple = heap.first();
-            if (minTriple.distance > 1.0) {
-                break;
-            }
             invalidateTriple(minTriple);
             final Community first = minTriple.first;
             final Community second = minTriple.second;
-            final Community mergedCommunity = mergeCommunities(first, second);
-            System.out.println("Merge " + first + " and " + second + " to " + mergedCommunity);
+            mergeCommunities(first, second);
         }
 
         for (Community community : communities) {
@@ -83,13 +77,6 @@ public class HAC extends Algorithm {
             }
         }
         return refactorings;
-    }
-
-    private Entity getRepresentative(Community community) {
-        if (community.entities.size() != 1) {
-            throw new IllegalArgumentException("Something went wrong! Singleton set expected");
-        }
-        return community.entities.iterator().next();
     }
 
     // todo doubtful code starts
@@ -111,30 +98,24 @@ public class HAC extends Algorithm {
     // doubtful code ends
 
     private Community mergeCommunities(Community first, Community second) {
-        final Set<Entity> merged = new HashSet<>();
-        merged.addAll(first.entities);
-        merged.addAll(second.entities);
-        final Set<String> availableClassNames = merged.stream()
-                .filter(e -> e.getCategory() == MetricCategory.Class)
-                .map(Entity::getName)
-                .collect(Collectors.toSet());
-        final String newName = merged.stream()
-                .collect(Collectors.groupingBy(Entity::getClassName, Collectors.counting()))
-                .entrySet().stream()
-                .filter(entry -> availableClassNames.contains(entry.getKey()))
-                .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .orElse(first.name);
+        final List<Entity> merged;
+        if (first.entities.size() < second.entities.size()) {
+            merged = second.entities;
+            merged.addAll(first.entities);
+        } else {
+            merged = first.entities;
+            merged.addAll(second.entities);
+        }
 
-        final Community newCommunity = new Community(merged, newName);
+        final Community newCommunity = new Community(merged);
         communities.remove(first);
         communities.remove(second);
 
         for (Community community : communities) {
             final Triple fromFirst = first.usages.get(community);
             final Triple fromSecond = second.usages.get(community);
-            final double newDistance = Math.max(fromFirst.distance, fromSecond.distance);
-            createAndInsertTriple(newDistance, newCommunity, community);
+            final double newDistance = Math.max(getDistance(fromFirst), getDistance(fromSecond));
+            insertTriple(newDistance, newCommunity, community);
             invalidateTriple(fromFirst);
             invalidateTriple(fromSecond);
         }
@@ -142,7 +123,14 @@ public class HAC extends Algorithm {
         return newCommunity;
     }
 
-    private void createAndInsertTriple(double distance, Community first, Community second) {
+    private double getDistance(Triple triple) {
+        return triple == null? Double.POSITIVE_INFINITY : triple.distance;
+    }
+
+    private void insertTriple(double distance, Community first, Community second) {
+        if (distance > 1.0) {
+            return;
+        }
         final Triple triple = new Triple(distance, first, second);
         second.usages.put(first, triple);
         first.usages.put(second, triple);
@@ -150,41 +138,33 @@ public class HAC extends Algorithm {
     }
 
     private void invalidateTriple(Triple triple) {
+        if (triple == null) {
+            return;
+        }
         triple.first.usages.remove(triple.second, triple);
         triple.second.usages.remove(triple.first, triple);
         heap.remove(triple);
     }
 
+    private Community singletonCommunity(Entity entity) {
+        final List<Entity> singletonList = new ArrayList<>(1);
+        singletonList.add(entity);
+        return new Community(singletonList);
+    }
+
     private class Community implements Comparable<Community> {
 
-        private final Set<Entity> entities;
+        private final List<Entity> entities;
         private final Map<Community, Triple> usages = new HashMap<>();
-        private final String name;
         private final long id;
 
-        Community(Set<Entity> entities, String name) {
+        Community(List<Entity> entities) {
             this.entities = entities;
-            this.name = name;
             id = idGenerator++;
-        }
-
-        Community(Entity entity) {
-            this(Collections.singleton(entity), entity.getName());
-        }
-
-        @Override
-        public String toString() {
-            return name + "(id = " + id + ")";
         }
 
         @Override
         public int compareTo(@NotNull Community o) {
-            if (o == this) {
-                return 0;
-            }
-            if (!name.equals(o.name)) {
-                return name.compareTo(o.name);
-            }
             return Long.compare(id, o.id);
         }
 
