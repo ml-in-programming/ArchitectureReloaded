ac64be2fa6c50a03da00025d20b7bfc4ccd5ec1b
ArtyomLobanov <lobanov98@yandex.ru>
2017-07-27 19:26:07 +0300

    Search definition in background

--- src/main/java/org/ml_methods_group/ui/ClassRefactoringPanel.java
+++ src/main/java/org/ml_methods_group/ui/ClassRefactoringPanel.java
@@ -17,7 +17,6 @@
 package org.ml_methods_group.ui;
 
 import com.intellij.analysis.AnalysisScope;
-import com.intellij.ide.util.EditorHelper;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.ScrollPaneFactory;
 import com.intellij.ui.TableSpeedSearch;
@@ -129,8 +128,7 @@ class ClassRefactoringPanel extends JPanel {
         if (selectedRow == -1 || selectedColumn == -1 || selectedColumn == SELECTION_COLUMN_INDEX) {
             return;
         }
-        PsiSearchUtil.findElement(model.getUnitAt(selectedRow, selectedColumn), scope)
-                .ifPresent(EditorHelper::openInEditor);
+        PsiSearchUtil.openDefinition(model.getUnitAt(selectedRow, selectedColumn), scope);
     }
 
     private void onSelectionChanged() {
