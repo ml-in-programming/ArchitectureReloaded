42a7471f1f7bb850a85d459d855a08193f7f6df6
RamSaw <mikhail@pravilov.com>
2018-07-09 17:58:45 +0300

    Erased bug with qualified names and added excluding short names like "as", "i" (as variable in for loop) by length, now accept only with length > 2.

--- src/main/java/org/ml_methods_group/algorithm/entity/RmmrEntitySearcher.java
+++ src/main/java/org/ml_methods_group/algorithm/entity/RmmrEntitySearcher.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Machine Learning Methods in Software Engineering Group of JetBrains Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ml_methods_group.algorithm.entity;
 
 import com.google.common.collect.Multiset;
@@ -59,6 +75,7 @@ public class RmmrEntitySearcher {
         strategy.setAcceptNewExpressions(false);
         strategy.setAcceptInnerClasses(true);
         strategy.setApplyStemming(true);
+        strategy.setMinimalTermLength(3);
         strategy.setCheckPsiVariableForBeingInScope(true);
     }
     /**
@@ -190,6 +207,7 @@ public class RmmrEntitySearcher {
         private void addIdentifierToBag(@Nullable Entity entity, String identifier) {
             if (entity != null) {
                 List<String> terms = IdentifierTokenizer.tokenize(identifier);
+                terms.removeIf(s -> s.length() < strategy.getMinimalTermLength());
                 if (strategy.isApplyStemming()) {
                     terms.replaceAll(s -> {
                         stemmer.add(s.toCharArray(), s.length());
@@ -211,9 +229,9 @@ public class RmmrEntitySearcher {
                 variablesClass = ((PsiClassType) variableType).resolve();
             }
             // TODO: add support for arrays int[][][][][].
-            if (isClassInScope(variablesClass) && variablesClass.getQualifiedName() != null) {
-                addIdentifierToBag(currentClasses.peek(), variablesClass.getQualifiedName());
-                addIdentifierToBag(currentMethod, variablesClass.getQualifiedName());
+            if (isClassInScope(variablesClass) && variablesClass.getName() != null) {
+                addIdentifierToBag(currentClasses.peek(), variablesClass.getName());
+                addIdentifierToBag(currentMethod, variablesClass.getName());
             }
             addIdentifierToBag(currentClasses.peek(), variableName);
             addIdentifierToBag(currentMethod, variableName);
--- src/main/java/org/ml_methods_group/algorithm/properties/finder_strategy/RmmrStrategy.java
+++ src/main/java/org/ml_methods_group/algorithm/properties/finder_strategy/RmmrStrategy.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Machine Learning Methods in Software Engineering Group of JetBrains Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ml_methods_group.algorithm.properties.finder_strategy;
 
 import com.intellij.psi.*;
@@ -23,6 +39,7 @@ public class RmmrStrategy implements FinderStrategy {
     private boolean acceptNewExpressions;
     private boolean acceptInnerClasses;
     private boolean applyStemming;
+    private int minimalTermLength;
 
     /**
      * Get instance of singleton object.
@@ -160,5 +177,13 @@ public class RmmrStrategy implements FinderStrategy {
 
     public boolean isApplyStemming() {
         return applyStemming;
+    }
+
+    public int getMinimalTermLength() {
+        return minimalTermLength;
+    }
+
+    public void setMinimalTermLength(int minimalTermLength) {
+        this.minimalTermLength = minimalTermLength;
     }
 }
