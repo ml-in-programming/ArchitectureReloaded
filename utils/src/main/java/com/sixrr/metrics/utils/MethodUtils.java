83168b238563b4f64f510bbce18feec6aa3415ed
RamSaw <mikhail@pravilov.com>
2018-07-09 16:35:34 +0300

    Removed qualified name error fix because tests were not passing.

--- utils/src/main/java/com/sixrr/metrics/utils/MethodUtils.java
+++ utils/src/main/java/com/sixrr/metrics/utils/MethodUtils.java
@@ -1,18 +1,17 @@
 /*
- * Copyright 2005-2016 Sixth and Red River Software, Bas Leijdekkers
- * Copyright 2017 Machine Learning Methods in Software Engineering Group of JetBrains Research
+ * Copyright 2018 Machine Learning Methods in Software Engineering Group of JetBrains Research
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.sixrr.metrics.utils;
@@ -92,9 +91,7 @@ public final class MethodUtils {
                 out.append(',');
             }
             final PsiType parameterType = parameters[i].getType();
-            // TODO: include qualified name
-            // final String parameterTypeText = parameterType.getPresentableText();
-            final String parameterTypeText = parameterType.getCanonicalText();
+            final String parameterTypeText = parameterType.getPresentableText();
             out.append(parameterTypeText);
         }
         out.append(')');
