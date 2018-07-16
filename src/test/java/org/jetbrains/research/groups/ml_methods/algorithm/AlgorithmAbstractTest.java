6fbaac2ae2ccc3c1cf7118bbaef4f20544770caa
Ivan Veselov <veselov.iva@gmail.com>
2018-07-16 14:46:13 +0300

    Extract ScopeAbstractTest.

--- src/test/java/org/jetbrains/research/groups/ml_methods/algorithm/AlgorithmAbstractTest.java
+++ src/test/java/org/jetbrains/research/groups/ml_methods/algorithm/AlgorithmAbstractTest.java
@@ -15,29 +15,17 @@ import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 @SuppressWarnings("WeakerAccess")
-public abstract class AlgorithmAbstractTest extends LightCodeInsightFixtureTestCase {
-    protected final TestCasesCheckers testCasesChecker = new TestCasesCheckers(getAlgorithm().getDescriptionString());
+public abstract class AlgorithmAbstractTest extends ScopeAbstractTest {
+    protected final TestCasesCheckers testCasesChecker =
+        new TestCasesCheckers(getAlgorithm().getDescriptionString());
 
-    @Override
-    protected String getTestDataPath() {
-        return "src/test/resources/testCases/" + getTestName(true);
-    }
-
-    @NotNull
-    private VirtualFile loadFile(@NotNull String name) {
-        final String fullName = getTestName(true) + "/" + name;
-        return myFixture.copyFileToProject(name, fullName);
-    }
-
-    protected AnalysisScope createScope(String... files) {
-        final List<VirtualFile> virtualFiles = Arrays.stream(files)
-                .map(this::loadFile)
-                .collect(Collectors.toList());
-        return new AnalysisScope(myFixture.getProject(), virtualFiles);
-    }
-
-    protected RefactoringExecutionContext createContext(AnalysisScope scope, Algorithm algorithm, Consumer<RefactoringExecutionContext> checker) {
-        MetricsProfile profile = MetricsProfilesUtil.createProfile("test_profile", algorithm.requiredMetrics());
+    protected RefactoringExecutionContext createContext(
+        AnalysisScope scope,
+        Algorithm algorithm,
+        Consumer<RefactoringExecutionContext> checker
+    ) {
+        MetricsProfile profile =
+            MetricsProfilesUtil.createProfile("test_profile", algorithm.requiredMetrics());
         return new RefactoringExecutionContext(myFixture.getProject(), scope, profile,
                 Collections.singletonList(algorithm), true,
                 checker);
---
+++ src/test/java/org/jetbrains/research/groups/ml_methods/algorithm/ScopeAbstractTest.java
@@ -0,0 +1,37 @@
+package org.jetbrains.research.groups.ml_methods.algorithm;
+
+import com.intellij.analysis.AnalysisScope;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class ScopeAbstractTest extends LightCodeInsightFixtureTestCase {
+    @Override
+    protected String getTestDataPath() {
+        return Paths.get(
+            "src",
+            "test",
+            "resources",
+            "testCases",
+            getTestName(true)
+        ).toString();
+    }
+
+    protected AnalysisScope createScope(String... files) {
+        final List<VirtualFile> virtualFiles = Arrays.stream(files)
+                .map(this::loadFile)
+                .collect(Collectors.toList());
+        return new AnalysisScope(myFixture.getProject(), virtualFiles);
+    }
+
+    @NotNull
+    private VirtualFile loadFile(@NotNull String name) {
+        final String fullName = Paths.get(getTestName(true), name).toString();;
+        return myFixture.copyFileToProject(name, fullName);
+    }
+}
