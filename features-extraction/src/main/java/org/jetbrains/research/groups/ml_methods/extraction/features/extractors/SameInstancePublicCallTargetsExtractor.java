cc083a6a3ee4892b660b17080518d8e9e83f476d
Ivan Veselov <veselov.iva@gmail.com>
2018-07-17 17:27:14 +0300

    Fix bug in SameInstancePublicCallTargetsExtractor.

--- features-extraction/src/main/java/org/jetbrains/research/groups/ml_methods/extraction/features/extractors/SameInstancePublicCallTargetsExtractor.java
+++ features-extraction/src/main/java/org/jetbrains/research/groups/ml_methods/extraction/features/extractors/SameInstancePublicCallTargetsExtractor.java
@@ -1,9 +1,11 @@
 package org.jetbrains.research.groups.ml_methods.extraction.features.extractors;
 
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiModifier;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.research.groups.ml_methods.extraction.info.MethodInfo;
 import org.jetbrains.research.groups.ml_methods.extraction.features.SameInstancePublicCallTargets;
+import org.jetbrains.research.groups.ml_methods.utils.MethodUtils;
 
 public class SameInstancePublicCallTargetsExtractor implements MoveMethodSingleFeatureExtractor {
     @Override
@@ -11,6 +13,8 @@ public class SameInstancePublicCallTargetsExtractor implements MoveMethodSingleF
         final @NotNull MethodInfo methodInfo,
         final @NotNull PsiClass targetClass
     ) {
-        return new SameInstancePublicCallTargets(methodInfo.getSameInstanceTargets().size());
+        return new SameInstancePublicCallTargets(
+            (int) methodInfo.getSameInstanceTargets().stream().filter(MethodUtils::isPublic).count()
+        );
     }
 }
