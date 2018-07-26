27ff84170e5b51923bc58018cf17f32536c0ece4
RamSaw <mikhail@pravilov.com>
2018-07-26 15:52:51 +0300

    Created IsOverridingMetric. Corrected NumOverloadsMetric.

--- stockmetrics/src/test/java/com/sixrr/stockmetrics/NumOverloadsMetricTest.java
+++ stockmetrics/src/test/java/com/sixrr/stockmetrics/NumOverloadsMetricTest.java
@@ -5,13 +5,14 @@ import com.sixrr.stockmetrics.methodMetrics.NumOverloadsMetric;
 
 public class NumOverloadsMetricTest extends MetricAbstractTest {
     public void testSimpleTest() {
+        String aPackage = "numOverloadsMetric.simpleTest";
         Metric metric = new NumOverloadsMetric();
-        MetricsResultsHolderTestImpl metricResults = runMetricOnTestCase(metric, "numOverloadsMetric/simpleTest/");
-        assertEquals(2.0, metricResults.getMethodMetric(metric, "numOverloadsMetric.simpleTest.ClassA.methodA1()"));
-        assertEquals(2.0, metricResults.getMethodMetric(metric, "numOverloadsMetric.simpleTest.ClassA.methodA1(int)"));
-        assertEquals(2.0, metricResults.getMethodMetric(metric, "numOverloadsMetric.simpleTest.ClassA.methodA1(double)"));
-        assertEquals(0.0, metricResults.getMethodMetric(metric, "numOverloadsMetric.simpleTest.ClassA.methodA2()"));
-        assertEquals(1.0, metricResults.getMethodMetric(metric, "numOverloadsMetric.simpleTest.ClassA.methodA3(int,double)"));
-        assertEquals(1.0, metricResults.getMethodMetric(metric, "numOverloadsMetric.simpleTest.ClassA.methodA3(double,int)"));
+        MetricsResultsHolderTestImpl metricResults = runMetricOnTestCase(metric, aPackage.replace('.', '/'));
+        assertEquals(2.0, metricResults.getMethodMetric(metric, aPackage + ".ClassA.methodA1()"));
+        assertEquals(2.0, metricResults.getMethodMetric(metric, aPackage + ".ClassA.methodA1(int)"));
+        assertEquals(2.0, metricResults.getMethodMetric(metric, aPackage + ".ClassA.methodA1(double)"));
+        assertEquals(0.0, metricResults.getMethodMetric(metric, aPackage + ".ClassA.methodA2()"));
+        assertEquals(1.0, metricResults.getMethodMetric(metric, aPackage + ".ClassA.methodA3(int,double)"));
+        assertEquals(1.0, metricResults.getMethodMetric(metric, aPackage + ".ClassA.methodA3(double,int)"));
     }
 }
---
+++ stockmetrics/src/test/resources/testCases/isOverridingMetric/simpleTest/ClassB.java
@@ -0,0 +1,8 @@
+package isOverridingMetric.simpleTest;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ClassB {
+    protected void methodA2() {
+    }
+}
