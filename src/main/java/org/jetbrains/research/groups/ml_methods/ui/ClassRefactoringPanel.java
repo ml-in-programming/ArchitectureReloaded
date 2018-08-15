ecc537112f895490be942ca37d1e7bfee007718c
ujohnny <eugeny.novozhilov@gmail.com>
2018-08-15 12:52:01 +0300

    fix refacroting list initialization

--- src/main/java/org/jetbrains/research/groups/ml_methods/ui/ClassRefactoringPanel.java
+++ src/main/java/org/jetbrains/research/groups/ml_methods/ui/ClassRefactoringPanel.java
@@ -22,6 +22,7 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -167,7 +168,7 @@ class ClassRefactoringPanel extends JPanel {
         selectAllButton.setEnabled(false);
         table.setEnabled(false);
         final List<MoveToClassRefactoring> selectedRefactorings = model.pullSelected().stream().map(CalculatedRefactoring::getRefactoring).collect(Collectors.toList());
-        final List<MoveToClassRefactoring> rejectedRefactorings = refactorings.stream().map(CalculatedRefactoring::getRefactoring).collect(Collectors.toList());
+        final List<MoveToClassRefactoring> rejectedRefactorings = new ArrayList<>();
         for (int index = 0; index < model.getRowCount(); ++index) {
             rejectedRefactorings.add(model.getRefactoring(index).getRefactoring());
         }
